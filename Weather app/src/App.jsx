d651ccaf1626dc71bc0aeec79709c02cc7d46d1c
Divya-Dharshini-31 <sdivyadharshini315@gmaail.com>
2024-09-05 19:53:20 +0530

    imported images

--- Weather app/src/App.jsx
+++ Weather app/src/App.jsx
@@ -1,6 +1,21 @@
 import { useState } from 'react'
-
 import './App.css'
+
+/*Import images*/
+import cloudyIcon from "./assets/cloudy.jpeg"
+import frostyIcon from "./assets/frosty.jpeg"
+import heavy_rainIcon from "./assets/heavy_rain.jpeg"
+import humidityIcon from "./assets/humidityicon.jpeg"
+import mistyIcon from "./assets/misty.jpeg"
+import overcastIcon from "./assets/overcast.jpeg"
+import partly_cloudyIcon from "./assets/partly_cloudy.jpeg"
+import rainyIcon from "./assets/rainy.jpeg"
+import searchIcon from "./assets/search.jpeg"
+import showersIcon from "./assets/showers.jpeg"
+import sunnyIcon from "./assets/sunny.jpeg"
+import thunderstormIcon from "./assets/thunderstorm.jpeg"
+import windIcon from "./assets/windicon.jpeg"
+
 
 function App() {
 
