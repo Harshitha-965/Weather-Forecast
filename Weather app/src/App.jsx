930027c4e0adb8f16032bc24044add6405e31ed5
Divya-Dharshini-31 <sdivyadharshini315@gmaail.com>
2024-09-05 20:47:01 +0530

    added display contents

--- Weather app/src/App.jsx
+++ Weather app/src/App.jsx
@@ -16,9 +16,41 @@ import sunnyIcon from "./assets/sunny.jpeg"
 import thunderstormIcon from "./assets/thunderstorm.jpeg"
 import windIcon from "./assets/windicon.jpeg"
 
+const WeatherDetails=({icon,temp,city,country,lat,long})=>{
+  return(<>
+    <div className="image">
+      <img src={icon} alt="cloudy"/>
+    </div>
+    <div className="temp">
+    {temp}&deg;C
+    </div>
+    <div className="location">
+    {city}
+    </div>
+    <div className="country">
+    {country}
+    </div>
+    <div className="cord">
+      <div>
+        <span className="lat">latitude</span>
+        <span>{lat}</span>
+      </div>
+      <div>
+        <span className="long">longitude</span>
+        <span>{long}</span>
+      </div>
+    </div>
+    </>
+    );
+};
 
 function App() {
-
+  const [icon,setIcon]=useState(cloudyIcon);
+  const [temp,setTemp]=useState(0);
+  const [city,setCity]=useState("Chennai");
+  const [country,setCountry]=useState("IN");
+  const [lat,setLat]=useState(0);
+  const [long,setLong]=useState(0);
   return (
     <>
       <div className="container">
@@ -30,9 +62,15 @@ function App() {
             <img src={searchIcon} alt="Search"/>
           </div>
         </div>
+        <WeatherDetails icon={icon} 
+        temp={temp} 
+        city={city}
+        country={country}
+        lat={lat}
+        long={long} />
       </div>
     </>
-  )
+  );
 }
 
 export default App
