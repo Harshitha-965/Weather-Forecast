fb9be8a8590f957490140d759faaa683724b3eb9
Harshitha-965 <harshithakg09@gmail.com>
2024-09-07 16:26:04 +0530

    updated

--- Weather app/src/App.jsx
+++ Weather app/src/App.jsx
@@ -16,7 +16,7 @@ import sunnyIcon from "./assets/sunny.jpeg"
 import thunderstormIcon from "./assets/thunderstorm.jpeg"
 import windIcon from "./assets/windicon.jpeg"
 
-const WeatherDetails=({icon,temp,city,country,lat,long})=>{
+const WeatherDetails=({icon,temp,city,country,lat,long,humidity,wind})=>{
   return(<>
     <div className="image">
       <img src={icon} alt="image"/>
@@ -40,25 +40,112 @@ const WeatherDetails=({icon,temp,city,country,lat,long})=>{
         <span>{long}</span>
       </div>
     </div>
+    <div className="data-container">
+      <div className="element">
+      <img src={humidityIcon} alt="humidity" className="icon"/>
+        <div className="data">
+          <div className="humidity-percent">{humidity}%</div>
+          <div className="text">Humidity</div>
+        </div>
+      </div>
+      <div className="element">
+        <img src={windIcon} alt="wind" className="icon"/>
+        <div className="data">
+          <div className="wind-percent">{wind}km/h</div>
+          <div className="text">Wind Speed</div>
+        </div>
+      </div>
+    </div>
     </>
     );
 };
 
+
 function App() {
+  let api_key="594b9aae4f0d18963145b6c4e8c80979";
+  const [text,setText]=useState("Chennai");
   const [icon,setIcon]=useState(cloudyIcon);
   const [temp,setTemp]=useState(0);
   const [city,setCity]=useState("Chennai");
   const [country,setCountry]=useState("IN");
   const [lat,setLat]=useState(0);
   const [long,setLong]=useState(0);
+  const [humidity,setHumidity]=useState(0);
+  const [wind,setWind]=useState(0);
+  const [cityNotFound,setCityNotFound]=useState(false);
+  const [loading,setLoading]=useState(false);
+  const weatherIconMap={
+    "01d":cloudyIcon,
+    "01n":cloudyIcon,
+    "02d":sunnyIcon,
+    "02n":sunnyIcon,
+    "03d":thunderstormIcon,
+    "03n":thunderstormIcon,
+    "04d":showersIcon,
+    "04n":showersIcon,
+    "09d":rainyIcon,
+    "09n":rainyIcon,
+    "10d":rainyIcon,
+    "10n":rainyIcon,
+    "13d":mistyIcon,
+    "13n":mistyIcon,
+  };
+
+  const search= async()=>{
+    setLoading(true);
+    let url=`https://api.openweathermap.org/data/2.5/weather?q=${text}&appid=${api_key}&units=Metric`;
+    try{
+      let res=await fetch(url);
+      let data=await res.json();
+      //console.log(data);
+      if(data.cod=="404"){
+        console.error("City Not Found");
+        setCityNotFound(true);
+        setLoading(false);
+        return;
+      }
+      setHumidity(data.main.humidity);
+      setWind(data.wind.speed);
+      setTemp(Math.floor(data.main.temp));
+      setCity(data.name);
+      setCountry(data.sys.country);
+      setLat(data.coord.lat);
+      setLong(data.coord.lon);
+      const weatherIconCode=data.weather[0].icon;
+      setIcon(weatherIconMap[weatherIconCode] || cloudyIcon);
+      setCityNotFound(false);
+    }
+    catch(error){
+      console.error("An error occured:",error.message);
+    }
+    finally{
+      setLoading(false);
+
+
+    }
+  };
+  const handleCity=(e)=>{
+    setText(e.target.value)
+
+  };
+  const handleKeyDown=(e)=>{
+    if(e.key=="Enter"){
+      search();
+    }
+
+  };
+  
+  
+
+
   return (
     <>
       <div className="container">
         <div className="input-container">
           <input type="text" 
           className="cityInput" 
-          placeholder="Search any city"/>
-          <div className="search-icon">
+          placeholder="Search any city" onChange={handleCity} value={text} onKeyDown={handleKeyDown}/>
+          <div className="search-icon" onClick={()=> search()}>
             <img src={searchIcon} alt="Search"/>
           </div>
         </div>
@@ -67,7 +154,12 @@ function App() {
         city={city}
         country={country}
         lat={lat}
-        long={long} />
+        long={long}
+        humidity={humidity}
+        wind={wind} />
+        <p className="copyright">
+          Designed By <span>DDH</span>
+        </p>
       </div>
     </>
   );
